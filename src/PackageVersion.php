9277aa635839926ebb9c3167c372d0bee0fc5fc9
Andreas Heigl <andreas@heigl.org>
2020-11-15 18:09:13 +0100

    Fix Package-Retrieval for Composer 2.0
    In composer 2.0 the way to retrieve the current packager changed so this fixes
    the issue by checking whether we are using composer v1 or composer v2 and using
    the appropriate way to retrieve the current package.

--- src/PackageVersion.php
+++ src/PackageVersion.php
@@ -15,11 +15,18 @@ declare(strict_types=1);
 
 namespace PharIo\ComposerDistributor;
 
+use Composer\Composer;
 use Composer\DependencyResolver\GenericRule;
+use Composer\DependencyResolver\Operation\InstallOperation;
+use Composer\DependencyResolver\Operation\UninstallOperation;
+use Composer\DependencyResolver\Operation\UpdateOperation;
 use Composer\Installer\PackageEvent;
 use Composer\Package\CompletePackage;
 use Composer\Semver\Constraint\MultiConstraint;
+use RuntimeException;
 use Throwable;
+use function var_dump;
+use function version_compare;
 
 final class PackageVersion
 {
@@ -93,17 +100,32 @@ final class PackageVersion
 
 	public static function fromPackageEvent(PackageEvent $event, string $pluginName) : self
 	{
-		/** @var GenericRule $rule */
-		$rule = $event->getOperation()->getReason();
-		/** @var MultiConstraint $constraint */
-		$constraint = $rule->getJob()['constraint'];
-		if ($rule->getRequiredPackage() !== $pluginName) {
-			throw SomebodyElsesProblem::here($pluginName);
+		$package = null;
+		if (0 >= version_compare('2.0.0', Composer::VERSION)) {
+			$operation = $event->getOperation();
+			switch (true) {
+				case $operation instanceof InstallOperation:
+				case $operation instanceof UninstallOperation:
+					$package = $operation->getPackage();
+					break;
+				case $operation instanceof UpdateOperation:
+					$package = $operation->getTargetPackage();
+					break;
+				default:
+					throw new RuntimeException('No valid operation found');
+			}
+		} else {
+			/** @var GenericRule $rule */
+			$rule = $event->getOperation()->getReason();
+			/** @var MultiConstraint $constraint */
+			$constraint = $rule->getJob()['constraint'];
+			if ($rule->getRequiredPackage() !== $pluginName) {
+				throw SomebodyElsesProblem::here($pluginName);
+			}
+
+			/** @var CompletePackage $packages */
+			$package = $event->getInstalledRepo()->findPackage($rule->getRequiredPackage(), $constraint->getPrettyString());
 		}
-
-		/** @var CompletePackage $packages */
-		$package = $event->getInstalledRepo()->findPackage($rule->getRequiredPackage(), $constraint->getPrettyString());
-
 		return new self($package->getName(), $package->getFullPrettyVersion());
 	}
 }
