ec99128d640e2ebef442e166f89015d0780bc0c8
expresso-virtual <47707426+expresso-virtual@users.noreply.github.com>
2022-09-04 18:31:47 -0300

    Fix deprecation notice on PHP 8.1

--- src/IteratorImplementation.php
+++ src/IteratorImplementation.php
@@ -13,26 +13,31 @@ trait IteratorImplementation
 {
     abstract public function &getList() : array;
 
+    #[\ReturnTypeWillChange]
     public function current()
     {
         return current($this->getList());
     }
 
+    #[\ReturnTypeWillChange]
     public function next()
     {
         next($this->getList());
     }
 
+    #[\ReturnTypeWillChange]
     public function key()
     {
         return key($this->getList());
     }
 
+    #[\ReturnTypeWillChange]
     public function valid()
     {
         return false === key($this->getList());
     }
 
+    #[\ReturnTypeWillChange]
     public function rewind()
     {
         return reset($this->getList());
